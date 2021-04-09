e185835d35377b328efba6aedc7943d487d26a6f
afelipe072 <andres.rojas.perez@correounivalle.edu.co>
2021-04-09 16:03:22 -0500

    pagina login

--- eventos-choclofront/src/App.js
+++ eventos-choclofront/src/App.js
@@ -6,6 +6,7 @@ Switch,
 Route} from 'react-router-dom'
 import Eventos from './components/pages/Eventos'
 import Footer from './components/Footer'
+import Login from './components/pages/Login'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home}/>   
           <Route path='/eventos' component={Eventos}/>
+          <Route path='/login' component={Login}/>
              
         </Switch> 
     </Router>
---
+++ eventos-choclofront/src/components/pages/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import FormularioLogin from '../FormularioLogin'
+
+
+function Login() {
+    return (
+        
+        <div className="container">
+            <h1 className="login">Login</h1>
+            <FormularioLogin/>
+          
+        </div>
+        
+        
+    )
+}
+
+export default Login
