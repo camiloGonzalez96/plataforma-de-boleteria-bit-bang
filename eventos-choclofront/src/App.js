e625c3da7f9bcab5d0a24292f03e4681b94edd9b
afelipe072 <andres.rojas.perez@correounivalle.edu.co>
2021-04-09 15:39:11 -0500

    footer

--- eventos-choclofront/src/App.js
+++ eventos-choclofront/src/App.js
@@ -4,6 +4,9 @@ import Navbar from './components/Navbar'
 import {BrowserRouter as Router,
 Switch,
 Route} from 'react-router-dom'
+import Eventos from './components/pages/Eventos'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
@@ -12,9 +15,11 @@ function App() {
       <Navbar/>   
         <Switch>
           <Route path='/' exact component={Home}/>   
+          <Route path='/eventos' component={Eventos}/>
              
         </Switch> 
     </Router>
+    <Footer/>
     </>
   );
 }
--- eventos-choclofront/src/components/Cards.js
+++ eventos-choclofront/src/components/Cards.js
@@ -5,7 +5,7 @@ import './Cards.css'
 function Cards() {
     return (
         <>
-        <h1>Eventos</h1>
+        <h1 className="titulo">Eventos</h1>
         <div className="container">
         
         <div className="row row-cols-1  row-cols-md-3 g-4">
